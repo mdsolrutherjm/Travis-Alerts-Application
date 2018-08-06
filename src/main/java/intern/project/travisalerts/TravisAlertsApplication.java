2dd1c8b9018d30de35acf7029663c0d9e31b84e6
jrutherford <jrutherford@mdsol.com>
2018-08-06 14:25:04 +0100

    Project Folder Setup

---
+++ src/main/java/intern/project/travisalerts/TravisAlertsApplication.java
@@ -0,0 +1,12 @@
+package intern.project.travisalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisAlertsApplication.class, args);
+	}
+}

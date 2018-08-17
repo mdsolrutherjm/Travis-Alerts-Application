04e80f244762b2a3a1304cdf57c240ef5ca12862
jrutherford <jrutherford@mdsol.com>
2018-08-17 17:40:00 +0100

    Now can be configued for any channel

--- src/main/java/intern/project/travisalerts/AppStatusRequestController.java
+++ src/main/java/intern/project/travisalerts/AppStatusRequestController.java
@@ -2,25 +2,21 @@ package intern.project.travisalerts;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.Iterator;
+import javax.xml.ws.Response;
 
 @RestController
 
 public class AppStatusRequestController {
+    private final String SLACKBTN = "<a href=\"https://slack.com/oauth/authorize?client_id=79629037168.408923971728&scope=incoming-webhook,commands\"><img alt=\"Add to Slack\" height=\"40\" width=\"139\" src=\"https://platform.slack-edge.com/img/add_to_slack.png\" srcset=\"https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x\" /></a>";
     @RequestMapping(value ="/app_status")
     public String app_status()
     {
@@ -35,14 +31,28 @@ public class AppStatusRequestController {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("client_id", "79629037168.408923971728" ); //REMOVE THIS LATER.
         map.add("client_secret", "e0d8e425514cad61362fc6167e93c386");
         map.add("code", code);
 
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map,headers);
 
-        restTemplate.postForEntity("https://slack.com/api/oauth.access", , String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity("https://slack.com/api/oauth.access", request, String.class);
+
+        System.out.println(response.getBody());
+        SlackAuthJSON slackAuth = JsonUtils.deserializeSlackAuth(response.getBody());
+
+        SlackNotifier sn = new SlackNotifier(slackAuth.incomingWebhook.channelURL);
+
+        //save the channel ID and channel URL to a text file.
+
+        sn.sendText("New Channel authenticated!");
         return "<h1>Slack Alerts Notifier</h1>\nProcessing your request... ";
-
+    }
+    @RequestMapping(value ="/newchannel")
+    public String newchannel()
+    {
+        return "<h2>Click below to add Travis Alerts to a new Medidata Slack channel!\n" + SLACKBTN;
     }
 }
--- src/main/java/intern/project/travisalerts/JsonUtils.java
+++ src/main/java/intern/project/travisalerts/JsonUtils.java
@@ -7,15 +7,26 @@ import java.io.IOException;
 public class JsonUtils {
 
     public static Branch deserializeJson(String inputJson){
-       ObjectMapper objectMapper = new ObjectMapper();
-       Branch branch = new Branch();
-       try {
-           branch = objectMapper.readValue(inputJson, Branch.class);
-       }
-       catch (IOException e){
+        ObjectMapper objectMapper = new ObjectMapper();
+        Branch branch = new Branch();
+        try {
+            branch = objectMapper.readValue(inputJson, Branch.class);
+        }
+        catch (IOException e){
 
-       }
+        }
         //System.out.println(branch.name + branch.defaultBranch +branch.existsOnGithub);
         return branch;
+    }
+    public static SlackAuthJSON deserializeSlackAuth(String inputJson){
+        ObjectMapper objectMapper = new ObjectMapper();
+        SlackAuthJSON slackAuth = new SlackAuthJSON();
+        try {
+            slackAuth = objectMapper.readValue(inputJson, SlackAuthJSON.class);
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+        return slackAuth;
     }
 }
